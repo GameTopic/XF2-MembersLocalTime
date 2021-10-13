bba0ecccc1c824604c972e3669643d5d7ea3afd6
NICK97 <bolandnick1997@gmail.com>
2021-10-12 19:28:05 -0700

    removed files

--- _output/extension_hint.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ################## THIS IS A GENERATED FILE ##################
-// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
-
-namespace ThemeHouse\MembersLocalTime\XF\Entity
-{
-	class XFCP_User extends \XF\Entity\User {}
-}
